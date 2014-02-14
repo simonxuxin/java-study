f03d61c2279e171ffcf064ac2f44a555303c68c8
Simon <xixu@paypal.com>
2014-02-14 15:28:04 +0800

    init commit

---
+++ src/main/java/study/tij/io/GetChannel.java
@@ -0,0 +1,34 @@
+package study.tij.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+
+/**
+ * Created by xixu on 2/14/14.
+ */
+public class GetChannel  {
+  private static final int BSIZE = 1024;
+  public static void main(String[] args) throws Exception {
+    FileChannel fc = new FileOutputStream("data.txt").getChannel();
+    fc.write(ByteBuffer.wrap("some text ".getBytes()));
+    fc.close();
+
+    fc = new RandomAccessFile("data.txt", "rw").getChannel();
+    fc.position(fc.size());
+    fc.write(ByteBuffer.wrap("Some more".getBytes()));
+    fc.close();
+
+    fc = new FileInputStream("data.txt").getChannel();
+    ByteBuffer buff = ByteBuffer.allocate(BSIZE);
+    fc.read(buff);
+    buff.flip();
+
+    String encoding = System.getProperty("file.encoding");
+    System.out.println(encoding);
+    System.out.println(Charset.forName(encoding).decode(buff));
+  }
+}
